a9e7d2ee12354de5ef658cf6026b01dc70303a59
fgibson94 <gibson.faithlee@yahoo.com>
2018-05-08 16:08:33 -0400

    needswork

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,59 +16,116 @@ var connection = mysql.createConnection({
 
 //create- 
 connection.connect();
-connection.query('SELECT *  FROM products', function (error, results, fields) {
-    console.log(results)
-});  
+function showInventory() {
 
-//Read- Prompt user to pick purchase inquirer checkbox choices from showInventory();
-inquirer.prompt([/* Show options to buy */]).then(answers => {    
-// showCart();
+    connection.query('SELECT *  FROM products', function (error, results, fields) {
+        //console.log(results)
+        console.log("===========================================")
+        for (var i = 0; i < results.length; i++) {
 
-//connection.query('UPDATE products WHERE isSelected = false to true', function (error, results, fields) { };
-//console.log(results) }); });
+            console.log(
+                results[i].item_id + " | " +
+                results[i].product_name + " | " +
+                " $" + results[i].price + " | " +
+                results[i].department_name + " | " +
+                results[i].stock_quantity + " In Stock" + "\n")
+        }
+        console.log("===========================================")
+    });
 
-function showInventory() {connection.query('SELECT *  FROM products', function (error, results, fields) {
-    if (error) throw error;
-    for (var i = 0; i < results.length; i++){
-        //inquirer(prompt)         
-        console.log (//results[i].id + "|" + results[i].title + "|" + results[i].price + "|" + results[i].quantity)
-        console.log ("===========================================")
-            }     
-        )}
+};
+var choicesArray = [];
+
+function selectedChoices() {
+    connection.query('SELECT *  FROM products', function (error, results, fields) {
+        if (error) throw error;
+        console.log("===========================================")
+        results.forEach(item => {
+            choicesArray.push(item.product_name)
+
+        });
+        console.log(choicesArray)
+        console.log("===========================================")
+
+    });
+
 };
 
-//Show page confirming purchases
-function showCart() {connection.query('SELECT * FROM products WHERE isSelected = true', function (error, results, fields){
-    if (error) throw error;
-    for (var i = 0; i < results.length; i++){
-        
-        console.log (//results[i].id + "|" + results[i].title + "|" + results[i].price + "|" + results[i].quantity)
-        console.log ("===========================================")
-};};};
+//selectedChoices();
 
-//Prompt continue shopping or checkout inquirer confirm ready to checkout or continue shopping;    
-            //1.showPurchases();    
-            //Cnt shopping returns to prev page
-            //2.showInventory();
-            //Update/Delete- checkout shows receipt
-function showPurchase(){
-            showCart(); 
-            console.log("purchases made"); 
-             //Decrease items quantity  
-    connection.query('UPDATE * FROM products DELETE quantity WHERE isSelected = true', function(error, results, fields){
-        if(error) throw error;     
-        showNewInventory();
-    };
- };
-                     
-//Update- shows shopping updated inventory
-function showNewInventory(){connection.query('SELECT * FROM products'), function(error, results, fields){
-        if (error) throw error;
-        for (var i = 0; i < results.length; i++){
-            //inquirer(prompt)
-            console.log (//results[i].id + "|" + results[i].title + "|" + results[i].price + "|" + results[i].quantity)
-            console.log ("===========================================")};
-                };
-               }; 
- //Prompt another purchase?
+//First Prompt to Allow User to Pick Items to purchase
+
+inquirer.prompt([
+    {
+        type: 'checkbox',
+        name: 'shopList',
+        message: 'What are you purchasing?',
+        choices: choicesArray
+    }
+
+])
+    .then(answers => {
+        console.log( JSON.stringify(answers.shopList))
+    });
+
+
+// //Show page confirming purchases
+// function showCart() {
+//     connection.query('SELECT * FROM products WHERE isSelected = true', function (error, results, fields) {
+//         if (error) throw error;
+//         for (var i = 0; i < results.length; i++) {
+
+//             console.log(//results[i].id + "|" + results[i].title + "|" + results[i].price + "|" + results[i].quantity)
+//                 console.log("===========================================")
+//             )
+//         };
+//     });
+// };
+
+// //Prompt continue shopping or checkout inquirer confirm ready to checkout or continue shopping;    
+// inquirer
+//     .prompt([
+//         {
+//             type: 'confirm',
+//             name: 'checkout',
+//             message: 'Would you like to checkout?'
+//         }
+//     ])
+//     .then(answers => {
+//         if (answers.checkout === true) {
+//             //1.showPurchases();//Update/Delete- checkout shows receipt
+//             console.log("purchase made")
+//         } else {
+//             //Cnt shopping returns to prev page
+//             //2.showInventory();
+//             console.log("keep shopping")
+//         }
+
+//     })
+
+
+// function showPurchase() {
+//     showCart();
+//     console.log("purchases made");
+//     //Decrease items quantity  
+//     connection.query('UPDATE * FROM products DELETE quantity WHERE isSelected = true', function (error, results, fields) {
+//         if (error) throw error;
+//         showNewInventory();
+//     }
+//     );
+// };
+
+// //Update- shows shopping updated inventory
+// function showNewInventory() {
+//     connection.query('SELECT * FROM products'), function (error, results, fields) {
+//         if (error) throw error;
+//         for (var i = 0; i < results.length; i++) {
+//             //inquirer(prompt)
+//             console.log(//results[i].id + "|" + results[i].title + "|" + results[i].price + "|" + results[i].quantity)
+//                 console.log("===========================================")
+//             )
+//         };
+//     };
+// };
+//Prompt another purchase?
  connection.end(); 
